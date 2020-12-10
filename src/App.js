0b9f42ae000b183defcd4f6d5379f9299b7fb8a1
松本一将 <k.matsumoto@benjmain.co.jp>
2020-12-10 17:54:42 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked!")}}/>
-      </div>
-    )
-  }
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 22 },
+    { name: "Hanako", age: 39 },
+    { name: 'NoName' }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name} {props.age} years old !</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
